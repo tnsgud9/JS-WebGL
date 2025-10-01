140640026cc18e77f5618b13c1995669dc05b181
SoonHyung_LEE <tnsgud9@naver.com>
2025-10-01 09:42:03 +0900

    update rotating square3 not completed

---
+++ RotatingSquare3/RotatingSquare3.js
@@ -0,0 +1,94 @@
+var gl;
+var points;
+var theta = 0.0;
+var thetaLoc;
+var delay = 100;
+var direction = true;
+var intervalId;
+
+window.onload = function init() {
+  var canvas = document.getElementById("gl-canvas");
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) {
+    alert("WebGL isn't available");
+  }
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+  var program = initShaders(gl, "vertex-shader", "fragment-shader");
+  gl.useProgram(program);
+
+  var vertices = [
+    vec2(0,1),
+    vec2(-1,0),
+    vec2(1,0),
+    vec2(0,-1)
+  ]
+
+  var vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+  var vPosition = gl.getAttribLocation(program, "vPosition");
+  gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
+  thetaLoc = gl.getUniformLocation(program, "theta");
+
+  document.getElementById("Direction").onclick = function(){
+    console.log(event.button);
+    direction = !direction;
+  }
+  document.getElementById("Controls").onchange = function(event){
+    console.log(event.target.value);
+    switch(event.target.value){
+      case "0":
+        direction = !direction;
+        break;
+      case "1":
+        delay /= 2.0;
+        clearInterval(intervalId);
+        intervalId = setInterval(render, delay);
+        break;
+      case "2":
+        delay *= 2.0;
+        clearInterval(intervalId);
+        intervalId = setInterval(render, delay);
+        break;
+    }
+  }
+
+  window.addEventListener("keydown", c2);
+  intervalId = setInterval(render, delay);
+};
+function render() {
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  theta += (direction ? 0.1 : -0.1);
+  gl.uniform1f(thetaLoc, theta);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4); // 0, 1, 2, 2, 1, 3
+  // requestAnimationFrame(render);
+}
+
+
+function c2(){
+  console.log("first event");
+  console.log(event.keyCode);
+  switch(event.keyCode){
+    case 49:
+      direction = !direction;
+      break;
+    case 50:
+      delay /= 2.0;
+      console.log(delay);
+      clearInterval(intervalId);
+      intervalId = setInterval(render, delay);
+      break;
+    case 51:
+      delay *= 2.0;
+      console.log(delay);
+      clearInterval(intervalId);
+      intervalId = setInterval(render, delay);
+      break;
+  }
+
+}
